20f1ec16c2716ab226d2ce55c0b2cd3057f53455
Sandumi Tharika <sandumi@cemexhrm.com>
2024-06-10 15:46:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'food-app';
+export class AppComponent implements OnInit{
+  constructor(private authService: AuthService){
+
+  }
+
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
+  
 }
